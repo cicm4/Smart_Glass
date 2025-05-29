c6ce8550aab55708ffbe84ace9d839a68fd9552c
cicm <camilocorreall44@gmail.com>
2025-05-29 10:44:41 -0400

    small changes and iterations to the blink model

--- blink_lstm_model.py
+++ blink_lstm_model.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 class BlinkLSTMNet(nn.Module):
-    def __init__(self, input_size=4, hidden_size=32, num_layers=1, num_classes=2):
+    def __init__(self, input_size=4, hidden_size=64, num_layers=3, num_classes=2):
         super().__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, num_classes)
